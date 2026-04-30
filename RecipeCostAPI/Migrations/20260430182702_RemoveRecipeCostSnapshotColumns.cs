9616c5635fa17aa1b74a9870e1ba33f216f8d630
Simon <sacost6@uic.edu>
2026-04-30 13:28:17 -0500

    Remove recipe cost snapshot columns

---
+++ RecipeCostAPI/Migrations/20260430182702_RemoveRecipeCostSnapshotColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeCostAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRecipeCostSnapshotColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CostPerServing",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCost",
+                table: "Recipes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CostPerServing",
+                table: "Recipes",
+                type: "numeric(18,4)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalCost",
+                table: "Recipes",
+                type: "numeric(18,4)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
